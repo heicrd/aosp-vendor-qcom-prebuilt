5ae4fcbad3d806850af3906698872c36dfe926a1
Adam Farden <adam@farden.cz>
2016-02-10 21:45:37 +0100

    Install only the device's camera sensor blobs

--- prebuilt/qcom-partial-msm8226.mk
+++ prebuilt/qcom-partial-msm8226.mk
@@ -30,102 +30,11 @@ PRODUCT_PACKAGES += \
 	libacdbloader \
 	libacdbmapper \
 	libacdbrtac \
-	libactuator_bu64291gwz \
-	libactuator_bu64291gwz_camcorder \
-	libactuator_bu64291gwz_camera \
-	libactuator_bu64296gwx \
-	libactuator_bu64296gwx_camcorder \
-	libactuator_bu64296gwx_camera \
-	libactuator_bu64747gwz \
-	libactuator_bu64747gwz_camcorder \
-	libactuator_bu64747gwz_camera \
 	libadiertac \
 	libadsprpc \
 	libaudcal \
 	libaudioalsa \
 	libchromaflash \
-	libchromatix_imx132_common \
-	libchromatix_imx132_cpp_liveshot \
-	libchromatix_imx132_cpp_preview \
-	libchromatix_imx132_cpp_snapshot \
-	libchromatix_imx132_cpp_video \
-	libchromatix_imx132_default_video \
-	libchromatix_imx132_liveshot \
-	libchromatix_imx132_postproc \
-	libchromatix_imx132_preview \
-	libchromatix_imx132_snapshot \
-	libchromatix_imx134_common \
-	libchromatix_imx134_cpp_hfr_120 \
-	libchromatix_imx134_cpp_hfr_60 \
-	libchromatix_imx134_cpp_hfr_90 \
-	libchromatix_imx134_cpp_liveshot \
-	libchromatix_imx134_cpp_preview \
-	libchromatix_imx134_cpp_snapshot \
-	libchromatix_imx134_cpp_video \
-	libchromatix_imx134_cpp_video_hd \
-	libchromatix_imx134_default_video \
-	libchromatix_imx134_hfr_120 \
-	libchromatix_imx134_hfr_60 \
-	libchromatix_imx134_hfr_90 \
-	libchromatix_imx134_liveshot \
-	libchromatix_imx134_postproc \
-	libchromatix_imx134_preview \
-	libchromatix_imx134_snapshot \
-	libchromatix_imx134_video_hd \
-	libchromatix_imx200_common \
-	libchromatix_imx200_cpp_hfr_120 \
-	libchromatix_imx200_cpp_hfr_60 \
-	libchromatix_imx200_cpp_hfr_90 \
-	libchromatix_imx200_cpp_liveshot \
-	libchromatix_imx200_cpp_preview \
-	libchromatix_imx200_cpp_snapshot \
-	libchromatix_imx200_cpp_snapshot_hdr \
-	libchromatix_imx200_cpp_video \
-	libchromatix_imx200_cpp_video_4k \
-	libchromatix_imx200_cpp_video_hdr \
-	libchromatix_imx200_default_video \
-	libchromatix_imx200_hfr_120 \
-	libchromatix_imx200_hfr_60 \
-	libchromatix_imx200_hfr_90 \
-	libchromatix_imx200_liveshot \
-	libchromatix_imx200_postproc \
-	libchromatix_imx200_preview \
-	libchromatix_imx200_snapshot \
-	libchromatix_imx200_snapshot_hdr \
-	libchromatix_imx200_video_4k \
-	libchromatix_imx200_video_hdr \
-	libchromatix_imx241_common \
-	libchromatix_imx241_cpp_liveshot \
-	libchromatix_imx241_cpp_preview \
-	libchromatix_imx241_cpp_snapshot \
-	libchromatix_imx241_cpp_video \
-	libchromatix_imx241_default_video \
-	libchromatix_imx241_liveshot \
-	libchromatix_imx241_postproc \
-	libchromatix_imx241_preview \
-	libchromatix_imx241_snapshot \
-	libchromatix_imx300_common \
-	libchromatix_imx300_cpp_hfr_120 \
-	libchromatix_imx300_cpp_hfr_60 \
-	libchromatix_imx300_cpp_hfr_90 \
-	libchromatix_imx300_cpp_liveshot \
-	libchromatix_imx300_cpp_preview \
-	libchromatix_imx300_cpp_snapshot \
-	libchromatix_imx300_cpp_snapshot_hdr \
-	libchromatix_imx300_cpp_video \
-	libchromatix_imx300_cpp_video_4k \
-	libchromatix_imx300_cpp_video_hdr \
-	libchromatix_imx300_default_video \
-	libchromatix_imx300_hfr_120 \
-	libchromatix_imx300_hfr_60 \
-	libchromatix_imx300_hfr_90 \
-	libchromatix_imx300_liveshot \
-	libchromatix_imx300_postproc \
-	libchromatix_imx300_preview \
-	libchromatix_imx300_snapshot \
-	libchromatix_imx300_snapshot_hdr \
-	libchromatix_imx300_video_4k \
-	libchromatix_imx300_video_hdr \
 	libconfigdb \
 	libdiag \
 	libdrmfs \
@@ -163,11 +72,6 @@ PRODUCT_PACKAGES += \
 	libmmcamera_faceproc \
 	libmmcamera_hdr_gb_lib \
 	libmmcamera_imglib \
-	libmmcamera_imx132 \
-	libmmcamera_imx134 \
-	libmmcamera_imx200 \
-	libmmcamera_imx241 \
-	libmmcamera_imx300 \
 	libmmcamera_isp_chroma_enhan40 \
 	libmmcamera_isp_chroma_suppress40 \
 	libmmcamera_isp_clamp_encoder40 \
@@ -235,3 +139,119 @@ PRODUCT_PACKAGES += \
 	libtrueportrait \
 	libubifocus \
 	libxml
+
+ifeq ($(CAMERA_IMX132),true)
+PRODUCT_PACKAGES += \
+	libchromatix_imx132_common \
+	libchromatix_imx132_cpp_liveshot \
+	libchromatix_imx132_cpp_preview \
+	libchromatix_imx132_cpp_snapshot \
+	libchromatix_imx132_cpp_video \
+	libchromatix_imx132_default_video \
+	libchromatix_imx132_liveshot \
+	libchromatix_imx132_postproc \
+	libchromatix_imx132_preview \
+	libchromatix_imx132_snapshot \
+	libchromatix_imx134_common \
+	libmmcamera_imx132
+endif
+
+ifeq ($(CAMERA_IMX134),true)
+PRODUCT_PACKAGES += \
+	libactuator_bu64291gwz \
+	libactuator_bu64291gwz_camcorder \
+	libactuator_bu64291gwz_camera \
+	libchromatix_imx134_cpp_hfr_120 \
+	libchromatix_imx134_cpp_hfr_60 \
+	libchromatix_imx134_cpp_hfr_90 \
+	libchromatix_imx134_cpp_liveshot \
+	libchromatix_imx134_cpp_preview \
+	libchromatix_imx134_cpp_snapshot \
+	libchromatix_imx134_cpp_video \
+	libchromatix_imx134_cpp_video_hd \
+	libchromatix_imx134_default_video \
+	libchromatix_imx134_hfr_120 \
+	libchromatix_imx134_hfr_60 \
+	libchromatix_imx134_hfr_90 \
+	libchromatix_imx134_liveshot \
+	libchromatix_imx134_postproc \
+	libchromatix_imx134_preview \
+	libchromatix_imx134_snapshot \
+	libchromatix_imx134_video_hd \
+	libmmcamera_imx134
+endif
+
+ifeq ($(CAMERA_IMX200),true)
+PRODUCT_PACKAGES += \
+	libactuator_bu64296gwx \
+	libactuator_bu64296gwx_camcorder \
+	libactuator_bu64296gwx_camera \
+	libchromatix_imx200_common \
+	libchromatix_imx200_cpp_hfr_120 \
+	libchromatix_imx200_cpp_hfr_60 \
+	libchromatix_imx200_cpp_hfr_90 \
+	libchromatix_imx200_cpp_liveshot \
+	libchromatix_imx200_cpp_preview \
+	libchromatix_imx200_cpp_snapshot \
+	libchromatix_imx200_cpp_snapshot_hdr \
+	libchromatix_imx200_cpp_video \
+	libchromatix_imx200_cpp_video_4k \
+	libchromatix_imx200_cpp_video_hdr \
+	libchromatix_imx200_default_video \
+	libchromatix_imx200_hfr_120 \
+	libchromatix_imx200_hfr_60 \
+	libchromatix_imx200_hfr_90 \
+	libchromatix_imx200_liveshot \
+	libchromatix_imx200_postproc \
+	libchromatix_imx200_preview \
+	libchromatix_imx200_snapshot \
+	libchromatix_imx200_snapshot_hdr \
+	libchromatix_imx200_video_4k \
+	libchromatix_imx200_video_hdr \
+	libmmcamera_imx200
+endif
+
+ifeq ($(CAMERA_IMX241),true)
+PRODUCT_PACKAGES += \
+	libchromatix_imx241_common \
+	libchromatix_imx241_cpp_liveshot \
+	libchromatix_imx241_cpp_preview \
+	libchromatix_imx241_cpp_snapshot \
+	libchromatix_imx241_cpp_video \
+	libchromatix_imx241_default_video \
+	libchromatix_imx241_liveshot \
+	libchromatix_imx241_postproc \
+	libchromatix_imx241_preview \
+	libchromatix_imx241_snapshot \
+	libmmcamera_imx241
+endif
+
+ifeq ($(CAMERA_IMX300),true)
+PRODUCT_PACKAGES += \
+	libactuator_bu64747gwz \
+	libactuator_bu64747gwz_camcorder \
+	libactuator_bu64747gwz_camera \
+	libchromatix_imx300_common \
+	libchromatix_imx300_cpp_hfr_120 \
+	libchromatix_imx300_cpp_hfr_60 \
+	libchromatix_imx300_cpp_hfr_90 \
+	libchromatix_imx300_cpp_liveshot \
+	libchromatix_imx300_cpp_preview \
+	libchromatix_imx300_cpp_snapshot \
+	libchromatix_imx300_cpp_snapshot_hdr \
+	libchromatix_imx300_cpp_video \
+	libchromatix_imx300_cpp_video_4k \
+	libchromatix_imx300_cpp_video_hdr \
+	libchromatix_imx300_default_video \
+	libchromatix_imx300_hfr_120 \
+	libchromatix_imx300_hfr_60 \
+	libchromatix_imx300_hfr_90 \
+	libchromatix_imx300_liveshot \
+	libchromatix_imx300_postproc \
+	libchromatix_imx300_preview \
+	libchromatix_imx300_snapshot \
+	libchromatix_imx300_snapshot_hdr \
+	libchromatix_imx300_video_4k \
+	libchromatix_imx300_video_hdr \
+	libmmcamera_imx300
+endif
