021d291cc2ac108693cc8ee08e3e434119584763
infectedmushi <zitoz@netvisao.pt>
2016-11-15 11:21:37 +0000

    SW_binaries_for_Xperia_AOSP_N_MR1_EXP_v01

---
+++ qcom-vendor.mk
@@ -0,0 +1,4 @@
+$(call inherit-product, vendor/qcom/prebuilt/qcom-partial-msm8952.mk)
+$(call inherit-product, vendor/qcom/prebuilt/qcom-partial-msm8996.mk)
+$(call inherit-product, vendor/qcom/prebuilt/qcom-partial-adreno-a5xx.mk)
+$(call inherit-product-if-exists, vendor/qcom/proprietary/common/build/qcom-packages.mk)
